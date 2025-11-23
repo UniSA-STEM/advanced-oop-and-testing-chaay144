006a155e7ef1a7c66cf1c320c6046bfe9e1659aa
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-11-23 19:39:20 +1030

    making a class name reptile.py

--- reptile.py
+++ reptile.py
@@ -6,3 +6,11 @@ ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from animal import Animal
+
+class Reptile(Animal):
+    def make_sound(self):
+        print(self.get_name() + " hisses softly.")
+
+    def shed_skin(self):
+        print(self.get_name() + " is sheds its skin.")
