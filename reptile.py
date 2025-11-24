425d417f0081a378e6ad135549acf6b28b321442
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-11-24 15:54:39 +1030

    Updating the reptile.py

--- reptile.py
+++ reptile.py
@@ -9,8 +9,10 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 from animal import Animal
 
 class Reptile(Animal):
+    # Reptile uses its own version of the sound method (polymorphism)
     def make_sound(self):
         print(self.get_name() + " hisses softly.")
 
+    # Special behaviour only reptiles have
     def shed_skin(self):
         print(self.get_name() + " is sheds its skin.")
