70fc3dba9336e6eff512897279e59c9b9a931fd1
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-11-24 16:00:37 +1030

    Updating the mammal.py

--- mammal.py
+++ mammal.py
@@ -9,8 +9,10 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 from animal import Animal
 
 class Mammal(Animal):
+    # Mammals have their own version of the sound they make (polymorphism)
     def make_sound(self):
         print(self.get_name() + " says roar.")
 
+    # Extra behaviour that only mammals have
     def feed_milk(self):
         print(self.get_name() + " is feeding milk to its young.")
